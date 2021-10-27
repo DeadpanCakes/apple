ca1db77fa21e1499cb98f6b18c2ae23efa11313b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-27 14:59:10 -0700

    styled hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,24 @@
+import "./Hero.css";
+
 const Hero = () => {
   return (
-    <div>
-      <hgroup>
-        <h1>What will your verse be?</h1>
-        <h2>
-          Everyone has something to share. See how people are using iPad in
-          their unique ways to add te the world's story.
-        </h2>
-        <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/#video-your-verse">
-          <h3>Watch the "Your Verse" film and explore the stories</h3>
-        </a>
-      </hgroup>
-    </div>
+    <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/">
+      <div className="hero">
+        <hgroup className="heroTxt">
+          <h1>What will your verse be?</h1>
+          <h2>
+            Everyone has something to share. See how people are using iPad in
+            their unique ways to add te the world's story.
+          </h2>
+          <a
+            className="heroLink"
+            href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/#video-your-verse"
+          >
+            <h3>Watch the "Your Verse" film and explore the stories</h3>
+          </a>
+        </hgroup>
+      </div>
+    </a>
   );
 };
 
