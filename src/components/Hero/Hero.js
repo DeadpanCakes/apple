fd5238e8cccffcef3de7ddb084507e31e912c934
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-27 13:49:22 -0700

    made hero markup

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div>
+      <hgroup>
+        <h1>What will your verse be?</h1>
+        <h2>
+          Everyone has something to share. See how people are using iPad in
+          their unique ways to add te the world's story.
+        </h2>
+        <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/#video-your-verse">
+          <h3>Watch the "Your Verse" film and explore the stories</h3>
+        </a>
+      </hgroup>
+    </div>
+  );
+};
+
+export default Hero;
