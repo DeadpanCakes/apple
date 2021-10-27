6dee2e7f450204b559d169ba334cf0261d53ff9e
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-27 13:51:43 -0700

    added markup to Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    return <header>
+        <nav>
+            <ul>
+                <li>Apple</li>
+                <li>Store</li>
+                <li>Mac</li>
+                <li>iPod</li>
+                <li>iPhone</li>
+                <li>iPad</li>
+                <li>iTunes</li>
+                <li>Support</li>
+            </ul>
+            <form>
+                <input />
+            </form>
+        </nav>
+    </header>
+}
+
+export default Header;
