35fd5559ccc296ae1d32889b1803227ff5228d88
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-27 14:11:38 -0700

    added markup to footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,74 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Shop the{" "}
+        <a href="https://web.archive.org/web/20140301004610/http://store.apple.com/us">
+          Apple Online Store
+        </a>{" "}
+        (1-800-MY-APPLLE), visit an{" "}
+        <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/retail/">
+          Apple Retail Store
+        </a>{" "}
+        or find a{" "}
+        <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/buy/">
+          reseller
+        </a>
+        .
+      </p>
+      <ul>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/sitemap/">
+            Site Map
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/hotnews/">
+            Hot News
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/rss/">
+            RSS Feeds
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/pr/">
+            Media Info
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/environment/">
+            Environment
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/jobs/">
+            Job Opportunities
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/contact/">
+            Contact Info
+          </a>
+        </li>
+        <li>
+          <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/choose-your-country/">
+            Locale
+          </a>
+        </li>
+      </ul>
+      <div>
+        <p>Copyright © 2014 Apple Inc. All rights reserved.</p>
+        <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/legal/">
+          Terms of Use
+        </a>
+        <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/legal/privacy/">
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
