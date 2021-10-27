bcdf38e4b5afcd611a3707ddacb07e9b5091697d
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-27 15:35:42 -0700

    styled app to layout page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,25 @@
-import "./Header.css"
+import "./Header.css";
 
 const Header = () => {
-    return <header>
-        <nav>
-            <ul className="navLinksContainer">
-                <li>Apple</li>
-                <li>Store</li>
-                <li>Mac</li>
-                <li>iPod</li>
-                <li>iPhone</li>
-                <li>iPad</li>
-                <li>iTunes</li>
-                <li>Support</li>
-            </ul>
-            <form>
-                <input />
-            </form>
-        </nav>
+  return (
+    <header>
+      <nav>
+        <ul className="navLinksContainer">
+          <li>Apple</li>
+          <li>Store</li>
+          <li>Mac</li>
+          <li>iPod</li>
+          <li>iPhone</li>
+          <li>iPad</li>
+          <li>iTunes</li>
+          <li>Support</li>
+        </ul>
+        <form>
+          <input />
+        </form>
+      </nav>
     </header>
-}
+  );
+};
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,7 +2,6 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <a href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/">
       <div className="hero">
         <hgroup className="heroTxt">
           <h1>What will your verse be?</h1>
@@ -10,15 +9,11 @@ const Hero = () => {
             Everyone has something to share. See how people are using iPad in
             their unique ways to add te the world's story.
           </h2>
-          <a
-            className="heroLink"
-            href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/#video-your-verse"
-          >
+          <a className="heroLink" href="https://web.archive.org/web/20140301004610/http://www.apple.com/your-verse/#video-your-verse">
             <h3>Watch the "Your Verse" film and explore the stories</h3>
           </a>
         </hgroup>
       </div>
-    </a>
   );
 };
 
